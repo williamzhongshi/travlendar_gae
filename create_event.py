9f39d0778f1bcc48ce8c1898df2b878f62acebc3
william <williamzhongshi@yahoo.com>
2017-11-19 12:26:51 -0600

    added warning if time is not enough

--- create_event.py
+++ create_event.py
@@ -4,6 +4,9 @@ import os
 import time, calendar
 import urllib
 import logging
+
+import re
+
 import cloudstorage as gcs
 from entities_def import CredentialsM
 
@@ -116,64 +119,101 @@ class CreateEvent(webapp2.RequestHandler):
             #
             # # original address hardcoded for now
             arrival_time_string_google = datetime.datetime.fromtimestamp(arrival_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
-            if depart_from_previous_dest:
-                print('Getting the upcoming 10 events')
-                now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-                logging.info("arrival_time_string_google: %s " % arrival_time_string_google)
-                eventsResult = service.events().list(
-                    calendarId='primary', timeMin=now, timeMax=arrival_time_string_google, singleEvents=True,
-                    orderBy='startTime').execute()
-                events = eventsResult.get('items', [])
-                if not events:
-                    logging.info('No upcoming events found.')
-                for event in reversed(events):
-                    start = event['start'].get('dateTime', event['start'].get('date'))
-                    # logging.info("Found event: %s" % event)
-                    logging.info("Found event: %s %s" % (start, event['summary']))
-                previous_event = events[-1]
-                if previous_event.get('location') is not None:
-                    logging.info("Previous location: %s" % (previous_event['location']))
-                    origin_address = previous_event['location']
-                else:
-                    origin_address = default_address
-            else:
-                origin_address = default_address
+            #if depart_from_previous_dest:
+            logging.info('Getting the upcoming 10 events')
+            now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+            logging.info("arrival_time_string_google: %s " % arrival_time_string_google)
+            eventsResult = service.events().list(
+                calendarId='primary', timeMin=now, timeMax=arrival_time_string_google, singleEvents=True,
+                orderBy='startTime').execute()
+            events = eventsResult.get('items', [])
+            if not events:
+                logging.info('No upcoming events found.')
+            for event in reversed(events):
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                # logging.info("Found event: %s" % event)
+                logging.info("Found event: %s %s" % (start, event['summary']))
+            previous_event = events[-1]
+
+            #    if previous_event.get('location') is not None:
+            # logging.info("Previous location: %s" % (previous_event['location']))
+            origin_address = previous_event['location']
+            # logging.info("Previous event %s " % previous_event)
+            origin_stop_time = previous_event['end']['dateTime']
+            t = re.search("(\d+-\d+-\d+T\d+:\d+:\d+)([+-]\d+):(\d+)", origin_stop_time)
+            origin_stop_time = t.group(1) + " %s" % time.tzname[0]
+            logging.info("Found stop time %s" % origin_stop_time)
+
+            origin_time_stamp = int(datetime.datetime.strptime(origin_stop_time, "%Y-%m-%dT%H:%M:%S %Z").strftime("%s"))
+            logging.info("Found previous event at %s, end at %s aka ts %d" % (origin_address, origin_stop_time, origin_time_stamp))
+            #    else:
+            #        origin_address = default_address
+            #else:
+            #    origin_address = default_address
 
             travel_time = self.get_estimated_time(origin_in=origin_address, destination_in=address,
                                                   arrival_time=arrival_time, transit_mode=transit_mode)
 
             departure_time = arrival_time - travel_time
-            logging.info("Arrival time is %s, departure time needs to be %s" % (arrival_time, departure_time))
+            if departure_time < origin_time_stamp:
+                logging.info("ERROR!!! not enough time to travel to next location")
+            else:
+                logging.info("Arrival time is %s, departure time needs to be %s" % (arrival_time, departure_time))
 
-            departure_time_string = datetime.datetime.fromtimestamp(departure_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
-            end_time_string = datetime.datetime.fromtimestamp(stop_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
-            logging.info("String departure time is %s" % departure_time_string)
+                departure_time_string = datetime.datetime.fromtimestamp(departure_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
+                arrival_time_string = datetime.datetime.fromtimestamp(arrival_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
+                end_time_string = datetime.datetime.fromtimestamp(stop_time).strftime('%Y-%m-%dT%H:%M:00-06:00')
+                logging.info("String departure time is %s" % departure_time_string)
 
-            event = {
-                'summary': event_name,
-                'location': address,
-                'description': 'test',
-                'start': {
-                    'dateTime': departure_time_string,
-                    'timeZone': 'America/Chicago',
-                },
-                'end': {
-                    'dateTime': end_time_string,
-                    'timeZone': 'America/Chicago',
-                },
-                'reminders': {
-                    'useDefault': False,
-                    'overrides': [
-                        {'method': 'email', 'minutes': 24 * 60},
-                        {'method': 'popup', 'minutes': 10},
-                    ],
-                },
-            }
+                event = {
+                    'summary': "Travel to %s" + event_name,
+                    'location': address,
+                    'description': 'test',
+                    'start': {
+                        'dateTime': departure_time_string,
+                        'timeZone': 'America/Chicago',
+                    },
+                    'end': {
+                        'dateTime': arrival_time_string,
+                        'timeZone': 'America/Chicago',
+                    },
+                    'reminders': {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 24 * 60},
+                            {'method': 'popup', 'minutes': 10},
+                        ],
+                    },
+                }
 
-            logging.info("Adding event %s " % event)
+                logging.info("Adding event %s " % event)
+                event = service.events().insert(calendarId='primary', body=event).execute()
+                logging.info('Travel event created: %s' % (event.get('htmlLink')))
+                event = {
+                    'summary': event_name,
+                    'location': address,
+                    'description': 'test',
+                    'start': {
+                        'dateTime': arrival_time_string,
+                        'timeZone': 'America/Chicago',
+                    },
+                    'end': {
+                        'dateTime': end_time_string,
+                        'timeZone': 'America/Chicago',
+                    },
+                    'reminders': {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 24 * 60},
+                            {'method': 'popup', 'minutes': 10},
+                        ],
+                    },
+                }
 
-            event = service.events().insert(calendarId='primary', body=event).execute()
-            print 'Event created: %s' % (event.get('htmlLink'))
+                logging.info("Adding event %s " % event)
+
+                event = service.events().insert(calendarId='primary', body=event).execute()
+                logging.info('Real event created: %s' % (event.get('htmlLink')))
 
 
 # [START app]
